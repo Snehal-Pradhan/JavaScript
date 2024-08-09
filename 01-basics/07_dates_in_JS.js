be64376b399d05d6120db0dd2b59cff3e7498caa
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-09 20:11:50 +0000

    learnt Date

---
+++ 01-basics/07_dates_in_JS.js
@@ -0,0 +1,32 @@
+/// dates
+
+let myDate = new Date()
+/* console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+ */
+
+
+// gives the following output in order.
+
+
+/* 
+2024-08-09T19:58:40.361Z
+Fri Aug 09 2024
+Fri Aug 09 2024 19:58:40 GMT+0000 (Coordinated Universal Time)
+19:58:40 GMT+0000 (Coordinated Universal Time)
+2024-08-09T19:58:40.361Z
+2024-08-09T19:58:40.361Z
+8/9/2024, 7:58:40 PM
+8/9/2024 
+*/
+console.log(typeof myDate);
+
+//let myCreateDate =new Date(2024,0,5);
+//let myCreateDate =new Date(2024,0,5,5,3);
+//console.log(myCreateDate.toLocaleString())
