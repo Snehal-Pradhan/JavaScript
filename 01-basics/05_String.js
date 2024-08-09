8ce4c14b4776c44f203499335665be927a46d8c9
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-09 19:22:26 +0000

    String Basics

---
+++ 01-basics/05_String.js
@@ -0,0 +1,34 @@
+const name = "somu"
+const repoCount =50;
+
+/* console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+//console.log(name+repoCount+"value");
+ */
+const gameName = new String('som4546u_abc');
+
+
+/* console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.toLowerCase);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('m')); */
+
+
+
+
+/* const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = gameName.slice(-5,2);
+console.log(anotherString);
+
+const newStringOne ="   somu   ";
+const new2String = newStringOne.trim();
+console.log(new2String);
+ */
+const url  ="https://somu.com/somu%20abc";
+console.log(url.replace('%20','-'));
+console.log(url.includes('somu'));
+console.log(gameName.split("4"));
