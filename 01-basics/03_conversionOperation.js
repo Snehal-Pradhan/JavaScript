f60cd411478baf3051544a62cef1731741f663c9
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-09 08:08:19 +0000

    learnt Operations

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = undefined;
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof (valueInNumber))
-console.log(valueInNumber)
+//console.log(typeof (valueInNumber))
+//console.log(valueInNumber)
 
 // "33" => 33
 // "33asd0" => NaN
@@ -20,5 +20,41 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 
 let someNumber =33;
 let StringNumber = String(someNumber);
-console.log(StringNumber)
-console.log(typeof StringNumber)
+//console.log(StringNumber)
+//console.log(typeof StringNumber)
+// ******************************* Operations *********************************
+ 
+let value = 3
+let negValue = -value;
+//console.log(negValue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 ="somu"
+let str3 = str1+" "+str2
+
+//console.log(str3);
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log(1+"2"+5);
+//console.log(1+5+"2");
+//console.log((3+4)*(5%3));
+
+//console.log(true);
+//console.log(+true);
+//console.log(+"") 
+// console.log(true+); => error
+
+/*let num1 , num2, num3
+num1 = num2 = num3 =2+2*/
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
+
+console.log(++gameCounter);
+console.log(gameCounter++);
