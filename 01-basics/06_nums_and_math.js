39f31e713d61ce55b7a17956078579d0700f0e80
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-09 19:49:52 +0000

    Number & Math

---
+++ 01-basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400;
+/* console.log(score); */
+
+const balance = new Number(100);
+/* console.log(balance);
+console.log(balance.toString.length); */
+/* console.log(balance.toFixed(2));
+ */
+
+
+
+/* 
+const otherNumber = 23.8945;
+console.log(otherNumber.toPrecision(3))
+ */
+
+//const hundreds =100000;
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.85));
+//console.log(Math.ceil(4.12));
+//console.log(Math.floor(4.85));
+
+
+//console.log(Math.min(4,5,6,4,8,9,15));
+//console.log(Math.max(5,1,3,4,9,7,8,6));
+//console.log(Math.floor(4.85));
+console.log(Math.floor((Math.random()*10)+1));
+
+// other method famous on Stack Overflow
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)));
