b6dc3c906d543c93911185122153fe52ce3e618e
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-12 14:14:14 +0000

    learnt scope level

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,10 +4,47 @@ if (true){
     let a = 10 ; 
     const b = 20;
     var c = 30;
-    console.log(`Inner Loop  : ${a}`)
+   // console.log(`Inner Loop  : ${a}`)
 }
-console.log(a);
+//console.log(a);
 //console.log(b);
-console.log(c); //=> works outside scope
+//console.log(c); //=> works outside scope
 
 // scope in browser console is different than the scope in node (global scope)
+
+function one(){
+    const username = "somu"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    //two();
+}
+ //one();
+
+ if(true){
+    const username = "somu";
+    if(username==="somu"){
+        const website ="youtube"
+        console.log(username+website);
+    }
+    //console.log(website)
+ }
+ //console.log(username)
+
+// -----------------interesting------------------
+console.log(addOne(5))
+function addOne(num){
+    return num+1;
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num+2
+}
+// two ways of declaring function.
+// using a variable method , we cannot use that funtion before.
+// more in hoisting and execution context.
+
