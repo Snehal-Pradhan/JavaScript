678e2b1824b4496f61d31e62feaa1c0b209a64c1
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-13 22:17:35 +0000

    learnt IF & ELSE

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+ // if 
+
+ if(true){
+
+ }
+ if(condition){
+    console.log("this line will never be printed.")  
+ }
+
+// < > , <= , >= , == ,!= ,!==, === => with type check also;
+// also learnt about if else;
