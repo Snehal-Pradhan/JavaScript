db78f661d622f83257d0a1b6aaa2ffdd97c44dc5
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-14 14:57:13 +0000

    learnt Switch

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,14 @@
+const userEmail ="abc@gmail.com";
+if(userEmail){
+    console.log("got user email");
+}
+else{
+    console.log("dont have user email")
+}
+
+
+// falsy values 
+// false ,0,-0 BigInt On,"",null,undefined,NaN 
+
+// truthy values
+// "0", 'false', " ", [],{}, funtion(){}
