dbac5b85c3a9f05d064653ba76e7b1fa96db132a
Snehal Pradhan <131527225+Snehal-Pradhan@users.noreply.github.com>
2024-08-15 06:56:19 +0000

    learnt While

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index <= 10){
+//     console.log(`Value Of Index is ${index}`);
+//     index = index+2
+// }
+let MyArray = ['ironman','spiderman','hulk']
+
+let arr =0
+while(arr<MyArray.length) {
+    console.log(`Value is ${MyArray[arr]}`)
+    arr++;
+}
+
+let score =11;
+do{
+    console.log(`score is ${score}`);
+    score++;
+}while(score <= 10)
+    // here do code segment is used first.
